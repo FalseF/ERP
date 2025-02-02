04668b92bc4eb00dc9df18df55ce5afc6cc16fb1
FalseF <busohag@gmail.com>
2025-02-02 21:34:22 +0600

    added ef repository , dependency registration class

---
+++ ERP System/ERP/Infrastructure/AddCors.cs
@@ -0,0 +1,20 @@
+﻿namespace ERP.Infrastructure
+{
+    public static class AddCors
+    {
+        public static IServiceCollection RegisterCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAnyOrigin",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin(); // Allow any origin
+                        builder.AllowAnyMethod(); // Allow any HTTP method
+                        builder.AllowAnyHeader(); // Allow any HTTP headers
+                    });
+            });
+            return services;
+        }
+    }
+}
